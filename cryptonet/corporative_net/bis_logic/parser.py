87581ebb0d033688e8ccc4db98f74eac3e31f201
astrhsptl <dctrback@bk.ru>
2022-05-02 08:32:21 -0400

    add eth & btc parser

---
+++ cryptonet/corporative_net/bis_logic/parser.py
@@ -0,0 +1,24 @@
+from webbrowser import get
+import requests
+
+from bs4 import BeautifulSoup
+
+def get_all_prices():
+    some = []
+    get_btc = requests.get('https://www.google.com/finance/quote/BTC-USD')
+    get_eth = requests.get('https://www.google.com/finance/quote/ETH-USD')
+    eth_soup = BeautifulSoup(get_eth.text, 'html.parser')
+    btc_soup = BeautifulSoup(get_btc.text, 'html.parser')
+    find_eth = eth_soup.findAll('div', class_='kf1m0')
+    find_btc = btc_soup.findAll('div', class_='kf1m0')
+        
+    for data in find_btc:
+        some.append(data.find('div', 'fxKbKc').text)
+
+    for data in find_eth:
+        some.append(data.find('div', 'fxKbKc').text)
+
+
+    return {'BTC': some[0] + '$', 'ETH': some[1] + '$'}
+
+print(get_all_prices())
